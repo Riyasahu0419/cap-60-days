d746fefe25cb9c61706a35a52678becd77370030
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2024-04-07 23:28:54 +0530

    its mini calculator

---
+++ dom/mini caculator/index.js
@@ -0,0 +1,36 @@
+
+
+let Addition =document.getElementById("add")
+let Substract = document.getElementById("Sub")
+let Multi = document.getElementById("Mul")
+let Division = document.getElementById("Divide")
+
+
+Addition.addEventListener("click", function () {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 + num2;
+    alert("Result: " + result);  
+  });
+  
+  Substract.addEventListener
+("click" , function(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 - num2;
+    alert( "result:",result)
+  } )
+  
+  Multi.addEventListener("click",function(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 * num2;
+    alert("result:",result)
+  })
+  
+  Division.addEventListener('click' , function(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 / num2
+    alert("result:",result)
+  })
