a3635aaf7ebfaae1bf68bf8639b113b3dd48fe87
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2024-04-24 22:49:14 +0530

    ...fetch

--- Fetch/Country App/index.js
+++ Fetch/Country App/index.js
@@ -6,7 +6,7 @@
 //         const res = await fetch("https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries")
 //         const data = await res.json()
 //         // console.log(data)
-//         fetchdata(data)
+//         fetchdata(data.data)
         
 //     }catch(err){
 //         console.log(err)
@@ -38,108 +38,40 @@
 
 
 
-const school = {
-    name: "XYZ School",
-    establishYear: 1990,
-    departments: {
-      math: { teachers: 5, students: 150 },
-      science: { teachers: 4, students: 120 },
-      history: { teachers: 3, students: 100 },
-      english: { teachers: 4, students: 130 },
-    },
-    courses: ["math", "science", "history", "english"],
-    students: [
-      {
-        name: "Alice",
-        className: "Grade 5",
-        scores: { math: 95, science: 88, history: 85, english: 92 },
-      },
-      {
-        name: "Bob",
-        className: "Grade 4",
-        scores: { math: 80, science: 78, history: 92, english: 85 },
-      },
-      {
-        name: "Charlie",
-        className: "Grade 5",
-        scores: { math: 88, science: 90, history: 78, english: 88 },
-      },
-      {
-        name: "Diana",
-        className: "Grade 4",
-        scores: { math: 92, science: 85, history: 88, english: 90 },
-      },
-    ],
-  };
-  
-  // Problem 10: countCalculation
-  
-  function countCalculation(school){
-      const {math: { teachers: mathTeachersCount, students: mathStudentsCount},
-              history: { teachers: historyTeachersCount, students: historyStudentsCount}} = school.departments
-              return { mathTeachersCount, historyTeachersCount, mathStudentsCount, historyStudentsCount, };
-  }
-  
-  
-  // Problem 11: findTopStudent
-  function findTopStudent(school, courseName){
-      const topStudent = school.students.reduce((prev, curr) => {
-          return curr.scores[courseName] > prev.scores[courseName] ? curr : prev;
-      });
-      return topStudent;
-  }
-  
-  
-  // Problem 12: addNewDept
-  function addNewDept(school, newDepartment){
-      const updatedSchool = {
-          ...school,
-          departments: {
-              ...school.departments,
-              ...newDepartment
-          }
-      };
-      return updatedSchool;
-  }
-  
-  // Problem 13: highestStudentCountDepartment
-  
-  function highestStudentCountDepartment(school){
-      let highestCount = 0;
-      let deptWithHighestCount = '';
-      for(const [dept, { students }] of Object.entries(school.departments)) {
-          if(students > highestCount) {
-              highestCount = students;
-              deptWithHighestCount = dept;
-          }
-      }
-      return deptWithHighestCount;
-  }
-  
-  
-  // Problem 14: generateGreeting
-  function generateGreeting(name, language = 'English'){
-      const greetings = {
-          English: `Hello, ${name}!`,
-          Spanish: `¡Hola, ${name}!`,
-          French: `Bonjour, ${name}!`
-  
-      };
-      return greetings[language];
-  
-  }
-  
-  
-  
-  
-  
-  
-  
-  console.log(countCalculation(school));
-  console.log(findTopStudent(school, 'math'));
-  const newDepartment = {arts: {teacher: 2, students: 50} };
-  console.log(addNewDept(school, newDepartment));
-  console.log(highestStudentCountDepartment(school));
-  console.log(generateGreeting("Alice"));
-  console.log(generateGreeting("Bob", "Spanish"));
-  console.log(generateGreeting("Charlie", "French"));
+let cont=document.getElementById('container')
+
+let selected=document.querySelector('#selected')
+
+selected.addEventListener('change',function(event){
+  let value=event.target.value
+  fetchdata(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries?sort=population&order=${value}`)
+})
+
+
+const fetchdata=async (url)=>{
+    let res=await fetch(url)
+    let data=await res.json()
+    countrydata(data.data)
+}
+
+
+function countrydata(arr){
+    cont.innerHTML=""
+    arr.forEach((ele)=>{
+        let div=document.createElement('div')
+        let h3=document.createElement('h3')
+
+        h3.innerText=`CountryName: ${ele.country}`
+        let h4=document.createElement('h4')
+        
+        h4.innerText=`Rank: ${ele.Rank}`
+        let para=document.createElement('p')
+
+        para.innerText=`Population: ${ele.population}`
+        div.append(h3,h4,para)
+        cont.append(div)
+    })
+    }
+
+fetchdata("https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries")
+
