aeea7e50d6ce05ab8d818d91cc4e921e1a75ad01
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2024-05-18 21:34:53 +0530

    react assignment-1

---
+++ React/Assignment7/React_basic_assignment/src/components/Greeting.jsx
@@ -0,0 +1,13 @@
+// src/components/Greeting.jsx
+
+import React from "react";
+
+function Greeting({ message }) {
+  return (
+    <div>
+      <h2>Hello, {/* complete the missing code */}{message}</h2>
+    </div>
+  );
+}
+
+export default Greeting;
